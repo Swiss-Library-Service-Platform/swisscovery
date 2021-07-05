59dfcc36b4d3bf2d774a3318f8ad112d06223c99
PhilippSLSP <83045137+PhilippSLSP@users.noreply.github.com>
2021-07-05 23:30:04 +0200

    Update slsp-courier-info.module.js

--- 41SLSP_NETWORK-VU1_UNION/js/slsp-courier-info/js/slsp-courier-info.module.js
+++ 41SLSP_NETWORK-VU1_UNION/js/slsp-courier-info/js/slsp-courier-info.module.js
@@ -13,18 +13,18 @@ angular
 
         this.$doCheck = function () {
             let form = false;
-            if (this.form.children[1].children[1] !== undefined &&
-        this.form.children[1].children[1].children[0] !== undefined) {
-                form = this.form.children[1].children[1].children[0];
+            let formLength = 2;
+            if (this.form.children[0].children[1] !== undefined && this.form.children[0].children[1].children[0] !== undefined) {
+                form = this.form.children[0].children[1].children[0];
             }
-            else if (this.form.children[1].children[0] !== undefined &&
-            this.form.children[1].children[0].children[0] !== undefined) {
-                form = this.form.children[1].children[0].children[0];
+            else if (this.form.children[0].children[0] !== undefined && this.form.children[0].children[0].children[0] !== undefined) {
+                form = this.form.children[0].children[0].children[0];
+                formLength = 3;
             }
 
             //create and insert info block if not present
 
-            if (form && form.children.length == 2) {
+            if (form && form.children.length == formLength) {
                 let info = document.createElement('DIV');
                 info.className = "courier-info bar alert-bar";
                 info.innerHTML =
@@ -32,14 +32,13 @@ angular
 		<p>${this.parentCtrl.$translate.instant('customize.fullview.feesInfo')}</p>
 		<p><a href="${this.parentCtrl.$translate.instant('customize.fullview.feesUrl')}"
 		target="_blank">${this.parentCtrl.$translate.instant('customize.fullview.feesLinkText')}</a></p>`;
-                form.insertBefore(info, form.children[1]);
+                form.insertBefore(info, form.children[formLength - 1]);
             }
         }
+
     }])
 
     .component('prmRequestAfter', {
         bindings: { parentCtrl: '<' },
         controller: 'CourierInfoController'
     });
-
-  
