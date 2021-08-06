11f7b186ae53dd8ea4e131ef51542880aacb21e6
Mehmet Celik <mehmet@celik.be>
2021-08-06 15:43:20 +0200

    fixes interfaceLanguage recognition.

--- 41SLSP_NETWORK-VU1_UNION/js/slsp-http-intercept-requests/js/slsp-http-intercept-requests.module.js
+++ 41SLSP_NETWORK-VU1_UNION/js/slsp-http-intercept-requests/js/slsp-http-intercept-requests.module.js
@@ -13,11 +13,10 @@ angular.module('slspHttpInterceptRequests', [])
                 },
                 'response': (response) => {
                     try {
-                        if (/primaws\/rest\/priv\/myaccount\/requests/.test(response.config.url)) {
-                            //We can also get the language from userSessionManagerService but it is costly
-                            let interfaceLanguage = window.appConfig['primo-view']['attributes-map'].interfaceLanguage
+                        if (/primaws\/rest\/priv\/myaccount\/requests/.test(response.config.url)) {                            
+                            let interfaceLanguage = angular.element(document.querySelector('primo-explore')).injector().get('$rootScope').$$childHead.$ctrl.userSessionManagerService.getUserLanguage() ||
+                                                    window.appConfig['primo-view']['attributes-map'].interfaceLanguage
                             
-                            //TODO: add translations
                             let list_of_hold_states = {
                                 'en': ["in transit", "process started", "on hold shelf"],
                                 'de': ["transfer", "bearbeitung gestartet", "bereitgestellt"],
@@ -26,18 +25,19 @@ angular.module('slspHttpInterceptRequests', [])
                             }
 
                             if (Object.keys(list_of_hold_states).includes(interfaceLanguage)) {
-
                                 let list_of_hold_states_regexp = RegExp(list_of_hold_states[interfaceLanguage].map(m => m.toLowerCase()).join('|'))
                                 if (response.status == 200 && response.data.status == "ok") {
+
                                     //rewrite all cancellable holds.
                                     response.data.data.holds.hold.map((m) => {
                                         if (list_of_hold_states_regexp.test(m.holdstatus.toLowerCase())) {
                                             m.cancel = 'N'
                                         }
                                     })
+
                                 }
                             } else {
-                                raise `No mapping found for ${interfaceLanguage}`
+                                throw `No mapping found for ${interfaceLanguage}`
                             }
                         }
                     } catch (error) {
