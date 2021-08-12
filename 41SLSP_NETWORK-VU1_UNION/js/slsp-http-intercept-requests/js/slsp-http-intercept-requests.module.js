45deb282dd0da721d81e0959fd1425942b39d05f
Mehmet Celik <mehmet@celik.be>
2021-08-12 10:03:16 +0200

    fixing uglify error

--- 41SLSP_NETWORK-VU1_UNION/js/slsp-http-intercept-requests/js/slsp-http-intercept-requests.module.js
+++ 41SLSP_NETWORK-VU1_UNION/js/slsp-http-intercept-requests/js/slsp-http-intercept-requests.module.js
@@ -1,50 +1,50 @@
-angular.module('slspHttpInterceptRequests', [])
+angular.module('slspHttpInterceptRequests', ['ng'])
     .config(['$httpProvider', ($httpProvider) => {
-        $httpProvider.interceptors.push(($q) => {
-            return {
-                'request': (request) => {
-                    return request
-                },
-                'requestError': (request) => {
-                    return $q.reject(request)
-                },
-                'responseError': (response) => {
-                    return $q.reject(response)
-                },
-                'response': (response) => {
-                    try {
-                        if (/primaws\/rest\/priv\/myaccount\/requests/.test(response.config.url)) {                            
-                            let interfaceLanguage = angular.element(document.querySelector('primo-explore')).injector().get('$rootScope').$$childHead.$ctrl.userSessionManagerService.getUserLanguage() ||
-                                                    window.appConfig['primo-view']['attributes-map'].interfaceLanguage
-                            
-                            let list_of_hold_states = {
-                                'en': ["in transit", "process started", "on hold shelf"],
-                                'de': ["transfer", "bearbeitung gestartet", "bereitgestellt"],
-                                'fr': ["en cours de transfert", "traitement lancé", "sur le rayon des réservations"],
-                                'it': ["in transito", "processo avviato", "scaffale prenotazioni"]
-                            }
+            $httpProvider.interceptors.push(['$q', ($q) => {
+                    return {
+                        'request': (request) => {
+                            return request
+                        },
+                        'requestError': (request) => {
+                            return $q.reject(request)
+                        },
+                        'responseError': (response) => {
+                            return $q.reject(response)
+                        },
+                        'response': (response) => {
+                            try {
+                                if (/primaws\/rest\/priv\/myaccount\/requests/.test(response.config.url)) {
+                                    let interfaceLanguage = angular.element(document.querySelector('primo-explore')).injector().get('$rootScope').$$childHead.$ctrl.userSessionManagerService.getUserLanguage() ||
+                                        window.appConfig['primo-view']['attributes-map'].interfaceLanguage
 
-                            if (Object.keys(list_of_hold_states).includes(interfaceLanguage)) {
-                                let list_of_hold_states_regexp = RegExp(list_of_hold_states[interfaceLanguage].map(m => m.toLowerCase()).join('|'))
-                                if (response.status == 200 && response.data.status == "ok") {
+                                    let list_of_hold_states = {
+                                        'en': ["in transit", "process started", "on hold shelf"],
+                                        'de': ["transfer", "bearbeitung gestartet", "bereitgestellt"],
+                                        'fr': ["en cours de transfert", "traitement lancé", "sur le rayon des réservations"],
+                                        'it': ["in transito", "processo avviato", "scaffale prenotazioni"]
+                                    }
 
-                                    //rewrite all cancellable holds.
-                                    response.data.data.holds.hold.map((m) => {
-                                        if (list_of_hold_states_regexp.test(m.holdstatus.toLowerCase())) {
-                                            m.cancel = 'N'
+                                    if (Object.keys(list_of_hold_states).includes(interfaceLanguage)) {
+                                        let list_of_hold_states_regexp = RegExp(list_of_hold_states[interfaceLanguage].map(m => m.toLowerCase()).join('|'))
+                                        if (response.status == 200 && response.data.status == "ok") {
+
+                                            //rewrite all cancellable holds.
+                                            response.data.data.holds.hold.map((m) => {
+                                                if (list_of_hold_states_regexp.test(m.holdstatus.toLowerCase())) {
+                                                    m.cancel = 'N'
+                                                }
+                                            })
+
                                         }
-                                    })
-
+                                    } else {
+                                        throw `No mapping found for ${interfaceLanguage}`
+                                    }
                                 }
-                            } else {
-                                throw `No mapping found for ${interfaceLanguage}`
+                            } catch (error) {
+                                console.log(error)
                             }
+                            return response
                         }
-                    } catch (error) {
-                        console.log(error)
                     }
-                    return response
-                }
-            }
-        })
-    }])
+                })
+            }])
