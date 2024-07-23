efc5624d3ea6edee0cef1ae81b6b57ef9818c47f
PhilippSLSP <philipp.schulz@slsp.ch>
2024-07-23 15:33:54 +0200

    Links to Switch update

--- 41SLSP_NETWORK-VU1_UNION/js/prm-union-catalog-login-after/slsp-union-login-box/slsp-union-login-box.controller.js
+++ 41SLSP_NETWORK-VU1_UNION/js/prm-union-catalog-login-after/slsp-union-login-box/slsp-union-login-box.controller.js
@@ -2,43 +2,33 @@
 
 export class slspUnionLoginBoxController {
 
-    constructor($scope, $compile, $timeout, $rootScope, $window) {
+    constructor($scope, $compile, $timeout) {
         this.$scope = $scope;
         this.$compile = $compile;
         this.$timeout = $timeout;
-        this.$rootScope = $rootScope;
-        this.$window = $window;
     }
 
     $onInit() {
         try {
+            
+                this.parentCtrl = this.afterCtrl.parentCtrl;
 
-            this.parentCtrl = this.afterCtrl.parentCtrl;
+                
+                console.log();
+                console.log(this.parentCtrl);
+                //console.log(this.parentCtrl.cancelLogin());
+                //console.log(this.parentCtrl.isUnionCatalogLogin());
 
 
-
-            //console.log(this.parentCtrl.directLoginService.userSessionManagerService.getInterfaceLanguage());
-            //console.log(this.parentCtrl.cancelLogin());
-            //console.log(this.parentCtrl.isUnionCatalogLogin());
-
-
-
+ 
         } catch (e) {
             console.error("***SLSP*** an error occured: Rapido hide Library\n\n");
             console.error(e.message);
         }
     }
     cancelUnionLogin() {
-        this.parentCtrl.cancelLogin();
-    }
-    getLanguage() {       
-        let sms = this.$rootScope.$$childHead.$ctrl.userSessionManagerService;
-        if (!sms) {
-            return 'en';
-        } else {
-            return sms.getUserLanguage() || $window.appConfig['primo-view']['attributes-map'].interfaceLanguage;
-        }
-    }
+                    this.parentCtrl.cancelLogin();
+                }
 }
 
-slspUnionLoginBoxController.$inject = ['$scope', '$compile', '$timeout', '$rootScope', '$window'];
+slspUnionLoginBoxController.$inject = ['$scope', '$compile', '$timeout'];
