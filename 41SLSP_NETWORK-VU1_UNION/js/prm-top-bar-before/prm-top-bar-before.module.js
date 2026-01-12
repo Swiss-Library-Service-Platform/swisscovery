8817dd274dc0174ff2a506801ed3da413aea1e2a
PhilippSLSP <philipp.schulz@slsp.ch>
2026-01-12 20:29:39 +0100

    Sprint 12 / 25

--- 41SLSP_NETWORK-VU1_UNION/js/prm-top-bar-before/prm-top-bar-before.module.js
+++ 41SLSP_NETWORK-VU1_UNION/js/prm-top-bar-before/prm-top-bar-before.module.js
@@ -1,6 +1,6 @@
 import { slspGitHintModule } from './slsp-git-hint/slsp-git-hint.module';
 import { slspKeyboardFocusModule } from "./slsp-keyboard-focus/slsp-keyboard-focus.module";
-//import { slspInfobarCloseButtonModule } from "./slsp-infobar-close-button/slsp-infobar-close-button.module";
+
 
 export const prmTopBarBeforeModule = angular
     .module('prmTopBarBeforeModule', [])
@@ -9,11 +9,9 @@ export const prmTopBarBeforeModule = angular
         template: `
             <slsp-git-hint-component after-ctrl="$ctrl"></slsp-git-hint-component>
             <slsp-keyboard-focus-component after-ctrl="$ctrl"></slsp-keyboard-focus-component>
-            <!--<slsp-infobar-close-button after-ctrl="$ctrl"></slsp-infobar-close-button>-->
             <slsp-top-bar-before parent-ctrl="$parent.$ctrl"></slsp-top-bar-before>`
     });
 
 
 prmTopBarBeforeModule.requires.push(slspGitHintModule.name);
 prmTopBarBeforeModule.requires.push(slspKeyboardFocusModule.name);
-//prmTopBarBeforeModule.requires.push(slspInfobarCloseButtonModule.name);
--- 41SLSP_NETWORK-VU1_UNION/js/prm-top-bar-before/slsp-infobar-close-button/slsp-infobar-close-button.module.js
+++
@@ -1,9 +0,0 @@
-import {slspInfobarCloseButtonController} from './slsp-infobar-close-button.controller';
-
-export const slspInfobarCloseButtonModule = angular
-    .module('slspInfobarCloseButtonModule', [])
-        .controller('slspInfobarCloseButtonController', slspInfobarCloseButtonController)
-        .component('slspInfobarCloseButtonComponent',{
-            bindings: {afterCtrl: '<'},
-            controller: 'slspInfobarCloseButtonController'
-        })
